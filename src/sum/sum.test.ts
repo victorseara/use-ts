2aa5cc15c2e645346f9bc31df2ef185e264ddbd7
victorseara <victorseara.me@gmail.com>
2024-05-13 23:19:44 -0300

    feat: create sum fn

---
+++ src/sum/sum.test.ts
@@ -0,0 +1,9 @@
+import { sum } from './sum';
+
+describe(sum.name, () => {
+  it('should sum two numbers', () => {
+    expect(sum(1, 2)).toBe(3);
+    expect(sum(2, 3)).toBe(5);
+    expect(sum(3, 4)).toBe(7);
+  });
+});
